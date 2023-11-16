d4182c5563ee7baefaaab6ca366e47a1ae99082c
nils <nilsotto@gmx.de>
2023-11-16 11:22:01 +0100

    add sayGoodbye

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -2,6 +2,6 @@ package de.neuefische;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Team");
+        System.out.println("Say Goodbye");
     }
 }
